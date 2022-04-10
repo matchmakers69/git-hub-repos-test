aeb2ff40317fa733a0086ad8f2e0ac1d0ba3fc87
PrzemekLewtak <przemek.lewtak@heywood.co.uk>
2022-04-10 21:06:02 +0100

    Updated method fetchRepos - added types and response check

--- src/services/API/fetchRepos.ts
+++ src/services/API/fetchRepos.ts
@@ -1,8 +1,8 @@
-import { Api } from "config/axios"
+import { get } from "config/axios"
 import { IRepo } from "types/repos";
 
 
 export const fetchReposBySearchQueries = async (searchQueries: string): Promise<IRepo> => {
-   const response = await Api.get(`${process.env.REACT_APP_BASE_API_URL}/search/repositories?${searchQueries}`);
+   const response = await get(`${process.env.REACT_APP_BASE_API_URL}/search/repositories?${searchQueries}`);
    return response?.data
 }
